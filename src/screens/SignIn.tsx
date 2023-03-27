c25070af2bdcf82fca045584f2fbc85f596df860
fearanha <felipearanha.c@gmail.com>
2023-03-27 11:44:14 -0300

    Tab Navigator

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,24 @@
 import { VStack, Image, Center, Text, Heading, ScrollView } from "native-base";
-
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { useNavigation } from "@react-navigation/native";
 
 export function SignIn() {
+  const navigation = useNavigation<AuthNavigatorRoutesProps>()
+
+  function handleNewAccount(){
+    navigation.navigate('signUp')
+  }
+
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1}} showsHorizontalScrollIndicator={false}>
-      <VStack flex={1} bg="gray.600" px={10} pb={16}>
+      <VStack flex={1} px={10} pb={16}>
         <Image
           source={BackgroundImg}
+          defaultSource={BackgroundImg}
           alt="Pessoas treinando"
           resizeMode="contain"
           position="absolute"
@@ -42,7 +50,7 @@ export function SignIn() {
           <Text color="gray.100" fontSize="sm" fontFamily="body" mb={3}>
             Ainda não tem acesso?
           </Text>
-          <Button title="Criar conta" variant="outline" />
+          <Button title="Criar conta" variant="outline" onPress={handleNewAccount} />
         </Center>
       </VStack>
     </ScrollView>
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -4,16 +4,24 @@ import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { useNavigation } from "@react-navigation/native";
 
 export function SignUp() {
+  const navigation = useNavigation()
+
+  function handleGoBack(){
+    navigation.goBack()
+  }
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
       showsHorizontalScrollIndicator={false}
     >
-      <VStack flex={1} bg="gray.600" px={10} pb={16}>
+      <VStack flex={1} px={10} pb={16}>
         <Image
           source={BackgroundImg}
+          defaultSource={BackgroundImg}
           alt="Pessoas treinando"
           resizeMode="contain"
           position="absolute"
@@ -44,7 +52,7 @@ export function SignUp() {
           <Button title="Criar e acessar" />
         </Center>
 
-        <Button mt={24} title="Voltar para o login" variant="outline" />
+        <Button mt={24} title="Voltar para o login" variant="outline"  onPress={handleGoBack} />
       </VStack>
     </ScrollView>
   );
