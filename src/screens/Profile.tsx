187f474d92312b77886d4beaa0094ea67424ae42
fearanha <felipearanha.c@gmail.com>
2023-03-28 17:18:58 -0300

    galeria de fotos | useForm

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,15 +1,62 @@
-import { Center, ScrollView, VStack, Skeleton, Text, Heading, useTheme } from "native-base";
+import {
+  Center,
+  ScrollView,
+  VStack,
+  Skeleton,
+  Text,
+  Heading,
+  useToast,
+} from "native-base";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { Input } from "@components/Input";
 import { UserPhoto } from "@components/UserPhoto";
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
 import { Button } from "@components/Button";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState("https://github.com/FeAranha.png");
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri
+        );
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            title: "Essa imagem utrapassou o limite de 5MB",
+            placement: "top",
+            bgColor: "red.500",
+          });
+        }
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -27,13 +74,13 @@ export function Profile() {
             />
           ) : (
             <UserPhoto
-              source={{ uri: "https://github.com/FeAranha.png" }}
+              source={{ uri: userPhoto }}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
           )}
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="green.500"
               fontWeight="bold"
@@ -48,8 +95,14 @@ export function Profile() {
           <Input bg="gray.600" placeholder="Nome" />
 
           <Input bg="gray.600" placeholder="E-mail" isDisabled />
-        
-          <Heading color="gray.200" fontSize="md" mb={2} alignSelf="flex-start" mt={12}> 
+
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            mb={2}
+            alignSelf="flex-start"
+            mt={12}
+          >
             Alterar senha
           </Heading>
 
@@ -65,7 +118,6 @@ export function Profile() {
 
           <Button title="Atualizar" mt={4} />
         </Center>
-
       </ScrollView>
     </VStack>
   );
