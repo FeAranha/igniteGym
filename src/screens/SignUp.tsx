af167407a08d6bbe8d64ede1ffb0fe26bd6bf23b
fearanha <felipearanha.c@gmail.com>
2023-03-29 20:22:58 -0300

    HTTP Client - Axios

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,38 +1,77 @@
-import { VStack, Image, Center, Text, Heading, ScrollView } from "native-base";
+import { Alert } from "react-native";
+import {
+  VStack,
+  Image,
+  Center,
+  Text,
+  Heading,
+  ScrollView,
+  useToast,
+} from "native-base";
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
 import { useForm, Controller } from "react-hook-form";
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { api } from "@services/api";
+import { AppError } from "@utils/AppError";
 
 type FormDataProps = {
   name: string;
   email: string;
   password: string;
   password_confirm: string;
-}
+};
 
 const signUpSchema = yup.object({
-  name: yup.string().required('Informe o nome.'),
-  email: yup.string().required('Informe o e-mail').email('E-mail inválido.'),
-  password: yup.string().required('Informe a senha').min(8, 'A senha deve ter pelo menos 8 dígitos.'),
-  password_confirm: yup.string().required('Confirme a senha.').oneOf([yup.ref('password')], 'A confirmação da senha não confere')
-})
+  name: yup.string().required("Informe o nome."),
+  email: yup.string().required("Informe o e-mail").email("E-mail inválido."),
+  password: yup
+    .string()
+    .required("Informe a senha")
+    .min(8, "A senha deve ter pelo menos 8 dígitos."),
+  password_confirm: yup
+    .string()
+    .required("Confirme a senha.")
+    .oneOf([yup.ref("password")], "A confirmação da senha não confere"),
+});
 
 export function SignUp() {
-  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
-    resolver: yupResolver(signUpSchema),})
+  const toast = useToast();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema),
+  });
 
   const navigation = useNavigation();
   function handleGoBack() {
     navigation.goBack();
   }
 
-  function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-    console.log({ name, email, password, password_confirm })
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    try {
+      const response = await api.post("/users", { name, email, password });
+      console.log(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+
+      const title = isAppError
+        ? error.message
+        : "Não foi possível criar a conta. Tente novamente mais tarde";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
   }
 
   return (
@@ -65,12 +104,12 @@ export function SignUp() {
             control={control}
             name="name"
             render={({ field: { onChange, value } }) => (
-              <Input 
-                placeholder="Nome" 
-                onChangeText={onChange} 
+              <Input
+                placeholder="Nome"
+                onChangeText={onChange}
                 value={value}
                 errorMessage={errors.name?.message}
-                />
+              />
             )}
           />
 
@@ -119,7 +158,10 @@ export function SignUp() {
             )}
           />
 
-          <Button title="Criar e acessar" onPress={handleSubmit(handleSignUp)} />
+          <Button
+            title="Criar e acessar"
+            onPress={handleSubmit(handleSignUp)}
+          />
         </Center>
 
         <Button
